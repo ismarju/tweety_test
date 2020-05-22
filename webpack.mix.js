2ae74cd77deeb550d3286c192e5c160be6251654
ismaju <ismar.julevic@gmail.com>
2020-05-23 00:39:12 +0200

    prva lekcija

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,8 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+
+// Ovo je CSS koji će biti kompajliran. Dodali smo sa tailwindcss stranice
+mix.postCss('resources/css/main.css', 'public/css', [
+require('tailwindcss'),
+])
